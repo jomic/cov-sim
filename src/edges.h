790e426eb3813b7aca152d3edebfe24437f29161
Christopher Rydell <stoffe@kiosan.com>
2020-11-12 21:25:28 +0100

    Added some documentation to edges header

--- src/edges.h
+++ src/edges.h
@@ -25,22 +25,56 @@ enum class contact_type : edge_t {
 	passerby = 0x4000
 };
 
+/*
+	Get the probability of an infected person infecting a susceptible person
+	within a single time step, if they have exactly one contact type.
+*/
 float get_probability();
 
+/*
+	Set the probability of an infected person infecting a susceptible person
+	within a single time step, if they have exactly one contact type.
+*/
 void set_probability(float prob);
 
+/*
+	Get an edge without any contact types.
+*/
 edge_t get_empty_edge();
 
+/*
+	Get a restriction without any contact types restricted.
+*/
 restriction_t get_empty_restriction();
 
+/*
+	Get the total number of contact types on an edge.
+*/
 int contact_type_count(edge_t *e);
 
+/*
+	Add a contact type to an edge.
+*/
 void add_contact_type(edge_t *e, contact_type c);
 
+/*
+	Remove a contact type from an edge.
+*/
 void remove_contact_type(edge_t *e, contact_type c);
 
+/*
+	Add a contact type to a restriction.
+*/
 void add_restriction(restriction_t *r, contact_type c);
 
+/*
+	Get the probability of an edge transmitting an infection with every
+	contact type on the edge taken into account.
+*/
 float infection_probability(edge_t *e);
 
+/*
+	Get the probability of an edge transmitting an infection with some 
+	contact types prevented with a restriction.
+*/
 float infection_probability(edge_t *e, restriction_t *r);
