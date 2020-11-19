986a3a1eada7b8ca5cbdcd58007013ce07adc8f7
Christopher Rydell <chry8424@siegbahn.it.uu.se>
2020-11-19 16:18:39 +0100

    Fixed errors in Individual

--- Individual.cpp
+++ Individual.cpp
@@ -1,68 +1,54 @@
 #include <vector>
 #include <iterator>
 #include "Graph.hpp"
+#include "Individual.hpp"
 
-class Individual {
-private:
-	int id;
-	bool s;
-	bool i;
-	bool r;
-	float susceptibility;
-	int infected_on;
-	Graph edges;
+// TODO: Might be a better way to randomize
+Individual::void try_infecting_neighbour(int t, int id, Graph& edges) {
+  Individual n = edges.get_node(id);
+  if (n.is_susceptible(t)) {
+    float roll = (float)rand() / (float)RAND_MAX;
+    if (roll < n.susceptibility)
+      n.infect(t);
+  }
+}
+Individual::Individual(int id) {
+  this->id = id;
+  s = true;
+  i = false;
+  r = false;
+  susceptibility = 0.01f;
+  infected_on = -1;
+}
 
+bool Individual::is_infected(int t) {
+  return i && t > infected_on;
+}
 
-	// TODO: Might be a better way to randomize
-	void try_infecting_neighbour(int t, int id) {
-		Individual n = edges.get_node(id);
-		if (n.is_susceptible(t)) {
-			float roll = (float)rand() / (float)RAND_MAX;
-			if (roll < n.susceptibility)
-				n.infect(t);
-		}
-	}
+bool Individual::is_susceptible(int t) {
+  return s;
+}
 
-public:
-	Individual(int id, Graph edges) {
-		this->id = id;
-		this->edges = edges;
-		s = true;
-		i = false;
-		r = false;
-		susceptibility = 0.01f;
-		infected_on = -1;
-	}
+void Individual::infect(int t) {
+  if (s) {
+    s = false;
+    i = true;
+    infected_on = t;
+  }
+}
 
-	bool is_infected(int t) {
-		return i && t > infected_on;
-	}
+void Individual::try_infecting_neighbours(int t, Graph& edges) {
+  if (!is_infected(t))
+    return;
+  std::vector<int> ns = edges.get_neighbours(id);
+  for (std::vector<int>::iterator it = ns.begin(); it != ns.end(); it++) {
+    try_infecting_neighbour(t, *it, edges);
+  }
+}
 
-	bool is_susceptible(int t) {
-		return s;
-	}
-
-	void infect(int t) {
-		if (s) {
-			s = false;
-			i = true;
-			infected_on = t;
-		}
-	}
-
-	void try_infecting_neighbours(int t) {
-		if (!is_infected(t))
-			return;
-		std::vector<int> ns = edges.get_neighbours(id);
-		for (std::vector<int>::iterator it = ns.begin(); it != ns.end(); it++) {
-			try_infecting_neighbour(t, *it);
-		}
-	}
-
-	void update_infection(int t, int d) {
-		if (i && duration < t - infected_on) {
-			i = false;
-			r = true;
-		}
-	}
-};
+void Individual::update_infection(int t, int d) {
+  if (i && d < t - infected_on) {
+    i = false;
+    r = true;
+  }
+}
--- Individual.hpp
+++ Individual.hpp
@@ -1,31 +1,42 @@
+#pragma once
 #include "Graph.hpp"
 
 class Individual {
+private:
+  int id;
+  bool s;
+  bool i;
+  bool r;
+  float susceptibility;
+  int infected_on;
+
+  void try_infecting_neighbour(int t, int id, Graph& edges);
+
 public:
-	Individual(int id, Graph edges); // TODO: I assume Graph should be a reference instead?
+  Individual(int id); // TODO: I assume Graph should be a reference instead?
 
-	/*
-		Check if the individual can infect people at time t.
-	*/
-	bool is_infected(int t);
+  /*
+    Check if the individual can infect people at time t.
+  */
+  bool is_infected(int t);
 
-	/*
-		Check if the individual can still be infected at time t.
-	*/
-	bool is_susceptible(int t);
+  /*
+    Check if the individual can still be infected at time t.
+  */
+  bool is_susceptible(int t);
 
-	/*
-		Infect the individual at time t.
-	*/
-	void infect(int t);
+  /*
+    Infect the individual at time t.
+  */
+  void infect(int t);
 
-	/*
-		Make the individual try to infect all of its neighbours at time t.
-	*/
-	void try_infecting_neighbours(int t);
+  /*
+    Make the individual try to infect all of its neighbours at time t.
+  */
+  void try_infecting_neighbours(int t, Graph& edges);
 
-	/*
-		Update the individuals infection, given that an infection last for d time steps.
-	*/
-	void update_infection(int t, int d);
+  /*
+    Update the individuals infection, given that an infection last for d time steps.
+  */
+  void update_infection(int t, int d);
 };
