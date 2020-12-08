19afd15ee4272e4960a6fececb694e7ed33b43d9
Christopher Rydell <chry8424@siegbahn.it.uu.se>
2020-12-08 10:47:05 +0100

    Added skeleton functions for initializer

---
+++ src/Initializer.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <iostream>
+#include "Group.hpp"
+#include "Simulator.hpp"
+#include "../include/json.hpp"
+
+using json = nlohmann::json;
+
+
+void get_groups_from_stream(std::istream& stream, std::vector<group_t>& groups) {
+  // TODO
+}
+
+void initialize_simulator_from_stream(std::istream& stream, Simulator& s) {
+  // TODO
+}
+
+void reset_stream(std::istream& stream) {
+  // TODO
+}
---
+++ src/Initializer.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include <vector>
+#include <iostream>
+#include "Group.hpp"
+#include "Simulator.hpp"
+
+/*
+  Fill a vector with groups based on information from json file
+ */
+void get_groups_from_stream(std::istream& stream, std::vector<group_t>& groups);
+
+/*
+  Set simulator parameters based on information from json file
+ */
+void initialize_simulator_from_stream(std::istream& stream, Simulator& s);
+
+/*
+  Reset an input stream so it can be used again
+ */
+void reset_stream(std::istream& stream);
