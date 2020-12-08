30be293ab3cb88433edcd98ee88ac036f48e4658
Christopher Rydell <chry8424@siegbahn.it.uu.se>
2020-12-08 14:17:14 +0100

    Implemented group parameter input

--- src/Initializer.cpp
+++ src/Initializer.cpp
@@ -4,16 +4,43 @@
 #include "Simulator.hpp"
 #include "../include/json.hpp"
 
+using namespace std;
 using json = nlohmann::json;
 
 
-void get_groups_from_stream(std::istream& stream, std::vector<group_t>& groups) {
+void get_groups_from_stream(istream& stream, vector<group_t>& groups) {
   json settings;
   stream >> settings;
-  // TODO
+
+  if (settings["groups"].is_array()) {
+    for (auto group : settings["groups"]) {
+      group_t g;
+      if (settings["n_i"].is_number())
+	g.n_i = settings["n_i"];
+      if (settings["n_ai"].is_number())
+	g.n_ai = settings["n_ai"];
+      if (settings["s"].is_number())
+	g.s = settings["s"];
+      if (settings["p_i"].is_number())
+	g.p_i = settings["p_i"];
+      if (settings["p_ai"].is_number())
+	g.p_ai = settings["p_ai"];
+      if (settings["p_v"].is_number())
+	g.p_v = settings["p_v"];
+      if (settings["d_v"].is_number())
+	g.d_v = settings["d_v"];
+      if (settings["d_i"].is_number())
+	g.d_i = settings["d_i"];
+      if (settings["d_ai"].is_number())
+	g.d_ai = settings["d_ai"];
+      if (settings["a_p"].is_number())
+	g.a_p = settings["a_p"];
+      groups.push_back(g);
+    }
+  }
 }
 
-void initialize_simulator_from_stream(std::istream& stream, Simulator& s) {
+void initialize_simulator_from_stream(istream& stream, Simulator& s) {
   json settings;
   stream >> settings;
 
@@ -27,7 +54,7 @@ void initialize_simulator_from_stream(std::istream& stream, Simulator& s) {
     s.n_v = settings["n_v"];
 }
 
-void reset_stream(std::istream& stream) {
+void reset_stream(istream& stream) {
   stream.clear();
   stream.seekg(0, stream.beg);
 }
--- src/cov-sim.cpp
+++ src/cov-sim.cpp
@@ -1,5 +1,6 @@
 #include <experimental/filesystem>
 #include <iostream>
+#include <vector>
 #include <unistd.h>
 #include "Graph.hpp"
 #include "Results.hpp"
@@ -9,16 +10,22 @@ using namespace std;
 
 int main() {
   cout << "path = " << experimental::filesystem::current_path() << endl;
-  Graph edges;
-  edges.input_from_file("ex_input_1.txt");
-  edges.print_graph();
-
   // Initialize a seed for the randomizer:
   srand(1);
   // srand(time(NULL));
 
+  vector<group_t> groups;
   Simulator s;
+
+  // Load groups and settings from stream
+  get_groups_from_stream(cin, groups);
+  reset_stream(cin);
   initialize_simulator_from_stream(cin, s);
+
+  cout << "Number of groups: " << groups.size() << endl;
+  Graph edges;
+  edges.input_from_file("ex_input_1.txt");
+  edges.print_graph();
 
   Results results = s.simulate(edges);
   results.print(8);
