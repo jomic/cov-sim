dc04c7d6e12e42d67b572cc1d5dd8c8b0c28d6aa
Christopher Rydell <chry8424@siegbahn.it.uu.se>
2020-12-15 15:16:41 +0100

    Fixed compilation issue

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -4,6 +4,7 @@
 #include <tuple>
 #include <vector>
 #include <memory>
+#include <algorithm>
 #include "Agent.hpp"
 #include "Graph.hpp"
 #include "Group.hpp"
@@ -91,6 +92,55 @@ void Graph::matrix_graph(int n, int d) {
     }
   }
 }
+
+
+bool roll(float p) {
+  return p > (float) rand() / (float) RAND_MAX;
+}
+
+void Graph::nw_small_world(int l, int k, float p) {
+  start_new_region();
+  
+  // Vector for storing connection vectors
+  vector<unique_ptr<vector<int>>> adjacencies;
+
+  // Create the initial adjacencies
+  for (int i = 0; i < l; i++) {
+    adjacencies.push_back(make_unique<vector<int>>());
+    for (int j = 1; j <= k; j++) {
+      adjacencies[i]->push_back((i + j) % l);
+      adjacencies[i]->push_back((i + l - j) % l);
+    }
+  }
+
+  // Create random adjacencies
+  for (int i = 0; i < l; i++) {
+    for (int j = i + k + 1; j < l; j++) {
+      if (j < i + l - k && roll(p)) {
+	adjacencies[i]->push_back(j);
+	adjacencies[j]->push_back(i);
+      }
+    }
+  }
+
+  // Sort each adjacency list
+#pragma omp parallel for
+  for (int i = 0; i < adjacencies.size(); i++)
+    sort(adjacencies[i]->begin(), adjacencies[i]->end());
+
+  // Store all adjacencies in the compressed row vectors
+  int id = 0;
+  int n_existing_agents = node_values.size();
+  int n_existing_connections = edges.size();
+  for (auto& adjacency : adjacencies) {
+    node_values.push_back(Agent(id++ + n_existing_agents));
+    offsets.push_back(n_existing_connections);
+    n_existing_connections += adjacency->size();
+    for (auto edge : *adjacency)
+      edges.push_back(edge + n_existing_agents);
+  }
+}
+
 
 void Graph::default_graph() {
   matrix_graph(20, 1);
