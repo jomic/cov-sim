328f9d7f32aa8e90504c2878a206126c8c77ff94
wezh <mrzhuwh@gmail.com>
2020-12-15 22:30:14 +0100

    controller editing

--- matrix/controllers/demo_v1_Test.h
+++ matrix/controllers/demo_v1_Test.h
@@ -13,7 +13,9 @@ class Test:public drogon::HttpController<Test>
     //METHOD_ADD(Test::get,"/{2}/{1}",Get);//path is /demo/v1/Test/{arg2}/{arg1}
     //METHOD_ADD(Test::your_method_name,"/{1}/{2}/list",Get);//path is /demo/v1/Test/{arg1}/{arg2}/list
     //ADD_METHOD_TO(Test::your_method_name,"/absolute/path/{1}/{2}/list",Get);//path is /absolute/path/{arg1}/{arg2}/list
-
+        METHOD_ADD(Test::login,"/token?userId={1}&passwd={2}",Post);
+        METHOD_ADD(Test::getInfo,"/{1}/info?token={2}",Get);
+        METHOD_ADD(Test::)
     METHOD_LIST_END
     // your declaration of processing function maybe like this:
     // void get(const HttpRequestPtr& req,std::function<void (const HttpResponsePtr &)> &&callback,int p1,std::string p2);
