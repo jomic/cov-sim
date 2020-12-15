cc25eba6c47c35773117332a326d871356937cc4
wezh <mrzhuwh@gmail.com>
2020-12-15 21:06:07 +0100

    add include folder

---
+++ matrix/include/Agent.cpp
@@ -0,0 +1,86 @@
+#include <vector>
+#include "Agent.hpp"
+#include "Graph.hpp"
+#include "Results.hpp"
+
+const group_t Agent::default_group;
+
+void Agent::try_infecting_neighbour(int t, int target_id, Graph& edges) {
+  Agent& n = edges.node_values[target_id];
+  if (n.is_susceptible(t)) {
+    float roll = (float)rand() / (float)RAND_MAX;
+    float risk;
+    if (i) {
+      risk = group.p_i * n.group.s;
+    } else {
+      risk = group.p_ai * n.group.s;
+    }
+    if (roll < risk)
+      n.infect(t);
+  }
+}
+
+Agent::Agent(int id, const group_t& group)
+  : id(id), group(group) {
+  s = true;
+  a = false;
+  i = false;
+  v = false;
+  r = false;
+  infected_on = -1;
+}
+
+Agent::Agent(int id)
+  : Agent::Agent(id, Agent::default_group) {}
+
+bool Agent::is_infected(int t) {
+  return (i || a) && t > infected_on;
+}
+
+bool Agent::is_susceptible(int t) {
+  return s;
+}
+
+void Agent::infect(int t) {
+  if (s) {
+    s = false;
+    float roll = (float)rand() / (float)RAND_MAX;
+    if (roll >= group.a_p) {
+      i = true;
+    } else {
+      a = true;
+    }
+    infected_on = t;
+  }
+}
+
+void Agent::try_infecting_neighbours(int t, Graph& edges) {
+  if (!is_infected(t))
+    return;
+  std::vector<int> n_ids = edges.neighbours(id);
+  for (auto n_id : n_ids)
+    try_infecting_neighbour(t, n_id, edges);
+}
+
+void Agent::update_infection(int t) {
+  if (i && infected_on + group.d_i < t) {
+    i = false;
+    r = true;
+  } else if (a && infected_on + group.d_ai < t) {
+    a = false;
+    r = true;
+  }
+}
+
+void Agent::update_results(int t, Results& results) {
+  if (s || ((i || a) && t == infected_on))
+    results.add_susceptible();
+  else if (a)
+    results.add_asymptomatic();
+  else if (i)
+    results.add_infected();
+  else if (v)
+    results.add_vaccinated();
+  else if (r)
+    results.add_removed();
+}
---
+++ matrix/include/Simulator.cpp
@@ -0,0 +1,45 @@
+#include "Graph.hpp"
+#include "Results.hpp"
+#include "Simulator.hpp"
+#include <vector>
+#include <set>
+#include <iostream>
+
+std::set<int> unique_random_numbers(int n, int max) {
+  std::set<int> numbers;
+  if (n > max) {
+    std::cout << "random_numbers: n cant be larger than total\n";
+    return numbers;
+  }
+  while (numbers.size() < n) {
+      numbers.insert(rand() % max);
+    }
+  return numbers;
+}
+
+void Simulator::infect_initial(Graph& edges, int n) {
+  std::set<int> rand_index = unique_random_numbers(n, edges.node_count());
+  for (auto i : rand_index) {
+    edges.node_values[i].infect(0);
+  }
+}
+
+void Simulator::iterate(Results& results, Graph& edges, int t) {
+  for (Agent &node : edges.node_values) {
+    node.update_results(t, results);
+    if (node.is_infected(t)) {
+      node.try_infecting_neighbours(t, edges);
+      node.update_infection(t);
+    }
+  }
+}
+
+Results Simulator::simulate(Graph& edges) {
+  Results results;
+  infect_initial(edges, initial_infections);
+  for (int t = 1; t <= t_end; t++) {
+    results.prepare_new_result();
+    iterate(results, edges, t);
+  }
+  return results;
+}
