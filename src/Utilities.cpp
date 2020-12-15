26008d215e94178c04fc1b7a62a1ae21bd4697a8
Jonatan Michalak <michalakjontan@gmail.com>
2020-12-15 19:39:04 +0100

    fix Utilities errors

--- src/Utilities.cpp
+++ src/Utilities.cpp
@@ -1,7 +1,8 @@
-#include <set>
 #include <vector>
+#include <set>
 #include <iostream>
 #include <string>
+#include "Utilities.hpp"
 
 std::set<int> unique_random_numbers(int n, int max) {
   std::set<int> numbers;
@@ -16,12 +17,12 @@ std::set<int> unique_random_numbers(int n, int max) {
 }
 
 // https://stackoverflow.com/a/46931770
-vector<string> split (string s, string delimiter) {
+std::vector<std::string> split (std::string s, std::string delimiter) {
   size_t pos_start = 0, pos_end, delim_len = delimiter.length();
-  string token;
-  vector<string> res;
+  std::string token;
+  std::vector<std::string> res;
 
-  while ((pos_end = s.find (delimiter, pos_start)) != string::npos) {
+  while ((pos_end = s.find (delimiter, pos_start)) != std::string::npos) {
     token = s.substr (pos_start, pos_end - pos_start);
     pos_start = pos_end + delim_len;
     res.push_back (token);
--- src/Utilities.hpp
+++ src/Utilities.hpp
@@ -4,6 +4,12 @@
 #include <vector>
 #include <string>
 
-vector<string> split (string s, string delimiter);
+/*
+  Returns a list of strings split by the delimiter
+*/
+std::vector<std::string> split (std::string s, std::string delimiter);
 
+/*
+  Returns n unique random integers between 0 and (excluding) max
+*/
 std::set<int> unique_random_numbers(int n, int max);
