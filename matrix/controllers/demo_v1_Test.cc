0253d8587c36a0d6bbe33d72c39dc7f614a0185a
wezh <mrzhuwh@gmail.com>
2020-12-16 01:05:59 +0100

    edit controller

--- matrix/controllers/demo_v1_Test.cc
+++ matrix/controllers/demo_v1_Test.cc
@@ -1,3 +1,75 @@
 #include "demo_v1_Test.h"
 using namespace demo::v1;
 //add definition of your processing function here
+
+void Test::login(const HttpRequestPtr &req,
+            std::function<void (const HttpResponsePtr &)> &&callback,
+            std::string &&userId,
+            const std::string &password)
+{
+    LOG_DEBUG<<"User "<<userId<<" login";
+
+    Json::Value ret;
+    ret["result"]="ok";
+    ret["token"]=drogon::utils::getUuid();
+    auto resp=HttpReponse::newHttpJsonResponse(ret);
+    callback(resp);
+}
+
+void Test::getInfo(const HttpRequestPtr &req,
+            std::function<void (const HttpResponsePtr &)> &&callback,
+            std::string userId,
+            const std::string &token) const
+{
+    LOG_DEBUG<<"User "<<userId<<" get his information";
+
+    Json::Value ret;
+    ret["result"]="ok!";
+    ret["user_name"]="Jack";
+    ret["user_id"]=userId;
+    ret["gender"]=1;
+    auto resp=HttpResponse::newHttpResponse(ret);
+    callback(resp);
+}
+
+void Test::setInit(const HttpRequestPtr &req,
+            std::function<void (const HttpResponsePtr &)> &&callback,
+            int sus,
+            int inf,
+            int rem)
+{
+    LOG_DEBUG<<"susceptable init: "<<sus;
+    
+    Json::Value ret;
+    ret["result"]="ok";
+    ret["susceptable"]=sus;
+    ret["infectious"]=inf;
+    ret["removed"]=rem;
+    auto resp=HttpResponse::newHttpResponse(ret);
+    callback(resp);
+}
+
+void Test::getResult(const HttpRequestPtr &req,
+            std::function<void (const HttpResponsePtr &)> &&callback) const 
+{
+    int L = 10;
+    int D_zero = 5;
+
+    srand(1);
+    Graph g;
+    g.matrix_graph(L, D_zero);
+
+    Simulator s;
+    Results results = s.simulator(g);
+    results.save_to_file("data.txt");
+    results.print(L*L);
+
+    std::vector<result_t> r = results.getResults();
+    
+    Json::Value ret;
+    ret["result"]="ok";
+    ret["status"]="final";
+    ret["final_susceptable"]=r[r.size()].s;
+    auto resp=HttpResponse::newHttpJsonResponse(ret);
+    callback(resp);
+}
--- matrix/controllers/demo_v1_Test.h
+++ matrix/controllers/demo_v1_Test.h
@@ -15,11 +15,27 @@ class Test:public drogon::HttpController<Test>
     //ADD_METHOD_TO(Test::your_method_name,"/absolute/path/{1}/{2}/list",Get);//path is /absolute/path/{arg1}/{arg2}/list
         METHOD_ADD(Test::login,"/token?userId={1}&passwd={2}",Post);
         METHOD_ADD(Test::getInfo,"/{1}/info?token={2}",Get);
-        METHOD_ADD(Test::)
+        METHOD_ADD(Test::setInit,"/s={1}/i={2}/r={3}",Post);
+        METHOD_ADD(Test::getResult,"/result",Get);
     METHOD_LIST_END
     // your declaration of processing function maybe like this:
     // void get(const HttpRequestPtr& req,std::function<void (const HttpResponsePtr &)> &&callback,int p1,std::string p2);
     // void your_method_name(const HttpRequestPtr& req,std::function<void (const HttpResponsePtr &)> &&callback,double p1,int p2) const;
+    void login(const HttpRequestPtr &req,
+            std::function<void (const HttpResponsePtr &)> &&callback,
+            std::string &&userId,
+            const std::string &password);
+    void getInfo(const HttpRequestPtr &req,
+            std::function<void (const HttpResponsePtr &)> &&callback,
+            std::string userId,
+            const std::string &token) const;
+    void setInit(const HttpRequestPtr &req,
+            std::function<void (const HttpResponsePtr &)> &&callback,
+            int sus,
+            int inf,
+            int rem);
+    void getResult(const HttpRequestPtr &req,
+            std::function<void (const HttpResponsePtr &)> &&callback) const;
 };
 }
 }
