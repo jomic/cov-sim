cecf3d2f4a4d8206099bd1b255a653a9ce8b4eb2
Christopher Rydell <chry8424@siegbahn.it.uu.se>
2021-01-04 13:38:03 +0100

    Fixed issues in density strategies

--- src/VacStrats.cpp
+++ src/VacStrats.cpp
@@ -1,6 +1,8 @@
 #include <algorithm>
 #include <iostream>
+#include <queue>
 #include <set>
+#include <utility>
 #include <vector>
 #include "Graph.hpp"
 #include "Utils.hpp"
@@ -36,4 +38,49 @@ void RandomStrategy::vaccinate(Graph& graf, int t) {
 
 void NothingStrategy::vaccinate(Graph& graf, int t) {
   return;
+}
+
+void DensityStrategy::vaccinate(Graph& graf, int t) {
+  if (t < time_deployed)
+    return;
+  
+  priority_queue<pair<int, int>> targets;
+  
+  for (int i = 0; i < graf.agents_count(); i++) {
+    if (graf.agents[i].can_be_vaccinated(t)) {
+      int priority = get_priority(graf, i, t);
+      targets.push(make_pair(priority, i));
+    }
+  }
+  
+  int n_vacs = std::min((int) targets.size(), vaccines_per_day);
+  for (int i = 0; i < n_vacs; i++) {
+    int target_id = targets.top().second;
+    graf.agents[target_id].vaccinate(t);
+    targets.pop();
+  }
+}
+
+int DensityStrategy::get_density(Graph& graf, int id, int t) {
+  int density = 0;
+  vector<int> ns = graf.neighbours(id);
+  for (int n : ns) {
+    if (graf.agents[n].is_infected(t))
+      density++;
+  }
+  return density;
+}
+
+int DensityStrategy::get_priority(Graph& graf, int id, int t) {
+  return 0;
+}
+
+int HighestDensityStrategy::get_priority(Graph& graf, int id, int t) {
+  int density = get_density(graf, id, t);
+  return density;
+}
+
+int LowestDensityStrategy::get_priority(Graph& graf, int id, int t) {
+  int density = get_density(graf, id, t);
+  return -density;
 }
