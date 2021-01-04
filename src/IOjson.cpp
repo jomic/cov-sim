a8098e7bd0184401f0cb571fba8736686cc4c6e9
Christopher Rydell <chry8424@siegbahn.it.uu.se>
2021-01-04 13:23:08 +0100

    Added highest and lowest density vac strategies

--- src/IOjson.cpp
+++ src/IOjson.cpp
@@ -31,6 +31,12 @@ void get_strategy(istream& stream, shared_ptr<VacStrat>& vs) {
     else if (json_strat["type"] == "random") {
       vs = make_shared<RandomStrategy>();
     }
+    else if (json_strat["type"] == "lowest_density") {
+      vs = make_shared<LowestDensityStrategy>();
+    }
+    else if (json_strat["type"] == "highest_density") {
+      vs = make_shared<HighestDensityStrategy>();
+    }
   }
 
   if (json_obj["T_v"].is_number())
--- src/VacStrats.hpp
+++ src/VacStrats.hpp
@@ -10,3 +10,21 @@ class NothingStrategy: public VacStrat {
 public:
   void vaccinate(Graph& graf, int t);
 };
+
+class DensityStrategy: public VacStrat {
+protected:
+  virtual int get_priority(Graph& graf, int id, int t);
+  int get_density(Graph& graf, int id, int t);
+public:
+  void vaccinate(Graph& graf, int t);
+};
+
+class LowestDensityStrategy: public DensityStrategy {
+protected:
+  int get_priority(Graph& graf, int id, int t);
+};
+
+class HighestDensityStrategy: public DensityStrategy {
+protected:
+  int get_priority(Graph& graf, int id, int t);
+};
