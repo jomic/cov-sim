08f3227b015cabc9d6802b8383f05eff50bb2756
wezh <mrzhuwh@gmail.com>
2021-01-07 17:04:01 +0100

    add

---
+++ backend/build/src/CMakeFiles/src.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/wezh/workplace/cov-project/cov-sim/backend/src/Agent.cpp" "/home/wezh/workplace/cov-project/cov-sim/backend/build/src/CMakeFiles/src.dir/Agent.cpp.o"
+  "/home/wezh/workplace/cov-project/cov-sim/backend/src/Graph.cpp" "/home/wezh/workplace/cov-project/cov-sim/backend/build/src/CMakeFiles/src.dir/Graph.cpp.o"
+  "/home/wezh/workplace/cov-project/cov-sim/backend/src/IOjson.cpp" "/home/wezh/workplace/cov-project/cov-sim/backend/build/src/CMakeFiles/src.dir/IOjson.cpp.o"
+  "/home/wezh/workplace/cov-project/cov-sim/backend/src/Results.cpp" "/home/wezh/workplace/cov-project/cov-sim/backend/build/src/CMakeFiles/src.dir/Results.cpp.o"
+  "/home/wezh/workplace/cov-project/cov-sim/backend/src/Simulator.cpp" "/home/wezh/workplace/cov-project/cov-sim/backend/build/src/CMakeFiles/src.dir/Simulator.cpp.o"
+  "/home/wezh/workplace/cov-project/cov-sim/backend/src/Utils.cpp" "/home/wezh/workplace/cov-project/cov-sim/backend/build/src/CMakeFiles/src.dir/Utils.cpp.o"
+  "/home/wezh/workplace/cov-project/cov-sim/backend/src/VacStrats.cpp" "/home/wezh/workplace/cov-project/cov-sim/backend/build/src/CMakeFiles/src.dir/VacStrats.cpp.o"
+  "/home/wezh/workplace/cov-project/cov-sim/backend/src/cov-sim.cpp" "/home/wezh/workplace/cov-project/cov-sim/backend/build/src/CMakeFiles/src.dir/cov-sim.cpp.o"
+  "/home/wezh/workplace/cov-project/cov-sim/backend/src/gen-graph.cpp" "/home/wezh/workplace/cov-project/cov-sim/backend/build/src/CMakeFiles/src.dir/gen-graph.cpp.o"
+  "/home/wezh/workplace/cov-project/cov-sim/backend/src/trivial.cpp" "/home/wezh/workplace/cov-project/cov-sim/backend/build/src/CMakeFiles/src.dir/trivial.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ backend/build/src/CMakeFiles/src.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/src.dir/Agent.cpp.o"
+  "CMakeFiles/src.dir/Graph.cpp.o"
+  "CMakeFiles/src.dir/IOjson.cpp.o"
+  "CMakeFiles/src.dir/Results.cpp.o"
+  "CMakeFiles/src.dir/Simulator.cpp.o"
+  "CMakeFiles/src.dir/Utils.cpp.o"
+  "CMakeFiles/src.dir/VacStrats.cpp.o"
+  "CMakeFiles/src.dir/cov-sim.cpp.o"
+  "CMakeFiles/src.dir/gen-graph.cpp.o"
+  "CMakeFiles/src.dir/trivial.cpp.o"
+  "libsrc.a"
+  "libsrc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/src.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
