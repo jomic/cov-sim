8eb9f14bec96fa81552cd22479f0abcbed6432ce
henrikdschulze <henrik.schulze.0353@student.uu.se>
2021-01-15 15:50:59 +0100

    Corrected an error in the printout.

--- src/GraphGeneration.cpp
+++ src/GraphGeneration.cpp
@@ -108,8 +108,7 @@ void Graph::nw_small_world(int N, int k, float p) {
 }
 
 void Graph::random_graph(int N, int N0) {
-  clog << endl << " N = " << N << " , N0 = " << N0
-       << " , Nchk1 = " << Nchk1 << " , Nchk2 = " << Nchk2 << endl;
+  clog << endl << " N = " << N << " , N0 = " << N0 << endl;
   vector<unique_ptr<vector<int>>> adjacencies;
   vector<int> unfilled;
   vector<int> degrees;
